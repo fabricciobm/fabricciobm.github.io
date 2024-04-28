fa4c95e6e4ae80919d15fe08709873fe6573a4c0
fabricciobm <fabricciobm@outlook.com>
2024-04-28 15:04:55 +0200

    update

--- src/app/pages/home/home.js
+++ src/app/pages/home/home.js
@@ -8,13 +8,13 @@ const Home = () => {
   return (
     <div className='home'>
         <LazyLoad height={200} offset={100}>
-        <section className='title-page-home lazy-background' style={{backgroundImage: `url(${LazyBackgroundImage})`}}>
-          <div className='container-fluid'>
-          <h2>Benvenuti</h2>
-          <p>Dove ogni progetto si trasforma in un'opera d'arte. Sono Fabriccio Martinelli, il vostro guida nell'esplorare il potenziale creativo e tecnologico.</p>
-          <Link to="/servizi" className="btn btn-trans btn-big">Inizia</Link>
-        </div>
-        </section>
+          <section className='title-page-home lazy-background' style={{backgroundImage: `url(${LazyBackgroundImage})`}}>
+            <div className='container-fluid'>
+            <h2>Benvenuti</h2>
+            <p>Dove ogni progetto si trasforma in un'opera d'arte. Sono Fabriccio Martinelli, il vostro guida nell'esplorare il potenziale creativo e tecnologico.</p>
+            <Link to="/servizi" className="btn btn-trans btn-big">Inizia</Link>
+          </div>
+          </section>
         </LazyLoad>
     </div>
   );
