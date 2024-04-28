cb575e297356fb39bcffd79660567dba7d153b62
fabricciobm <fabricciobm@outlook.com>
2024-04-28 15:00:01 +0200

    update

--- src/app/pages/home/home.js
+++ src/app/pages/home/home.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './../../styles/home.css';
+import LazyLoad from 'react-lazyload';
 import LazyBackgroundImage from './../../assets/background.webp';
+import './../../styles/home.css';
 
 const Home = () => {
   return (
     <div className='home'>
-      <section className='title-page-home' style={{backgroundImage: `url(${LazyBackgroundImage})`}}>
-        <div className='container-fluid'>
+        <LazyLoad height={200} offset={100}>
+        <section className='title-page-home lazy-background' style={{backgroundImage: `url(${LazyBackgroundImage})`}}>
+          <div className='container-fluid'>
           <h2>Benvenuti</h2>
           <p>Dove ogni progetto si trasforma in un'opera d'arte. Sono Fabriccio Martinelli, il vostro guida nell'esplorare il potenziale creativo e tecnologico.</p>
           <Link to="/servizi" className="btn btn-trans btn-big">Inizia</Link>
         </div>
-      </section>
+        </section>
+        </LazyLoad>
     </div>
   );
 }
