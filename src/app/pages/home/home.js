140bc0fc9a97e5c376cf4dcbcdd5cee52abf453e
fabricciobm <fabricciobm@outlook.com>
2024-04-28 12:49:56 +0200

    update

--- src/app/pages/home/home.js
+++ src/app/pages/home/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './../../styles/home.css';
-import imagem from './../../assets/eu.webp';
+import imagem from './../../assets/eux.webp';
 
 const Home = () => {
   return (
