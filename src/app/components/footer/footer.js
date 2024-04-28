acbc08f15c8787050751c089cdd3f400b45d5b44
fabricciobm <fabricciobm@outlook.com>
2024-04-28 12:39:22 +0200

    update

--- src/app/components/footer/footer.js
+++ src/app/components/footer/footer.js
@@ -1,17 +1,16 @@
 // Footer.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './../../styles/footer.css';
 import FooterColumn1 from './footerColumn1'; 
 import FooterColumn2 from './footerColumn2'; 
 import FooterColumn3 from './footerColumn3';
 import icons from './../../components/icons.js';
-import { Link } from 'react-router-dom';
-
 
 const Footer = () => {
   return (
     <footer className="app-footer">
-      <Link to="/servizi" className="go-to-shop">
+      <Link to="/servizi" className="go-to-shop" aria-label="Vai al carrello">
         {icons.shop()}
       </Link>
       <div className='container-fluid'>
