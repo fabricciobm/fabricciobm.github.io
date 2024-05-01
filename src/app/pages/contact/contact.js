d74728057d22df693ffe44f2459ec7966f78bae2
fabricciobm <fabricciobm@outlook.com>
2024-05-01 17:16:13 +0200

    update

--- src/app/pages/contact/contact.js
+++ src/app/pages/contact/contact.js
@@ -1,40 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './../../styles/contact.css';
-import iconComponents from './../../components/icons';
+import iconComponents from './../../components/icons'; 
 import LazyBackgroundImage from './../../assets/service1.webp';
-import emailjs from 'emailjs-com';
-
 const Contact = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [submitStatus, setSubmitStatus] = useState(null);
-
-  const sendEmail = async (e) => {
-    e.preventDefault();
-
-    setIsSubmitting(true);
-
-    try {
-      const result = await emailjs.sendForm(
-        'service_44bzcfh', 
-        'template_okd4jtn',
-        e.target,
-        'x8lrLLuHnsxk_vP77'
-      );
-
-      console.log(result.text);
-      setSubmitStatus('success');
-
-    } catch (error) {
-      console.error('Erro ao enviar e-mail:', error);
-      setSubmitStatus('error');
-    }
-
-    setIsSubmitting(false);
-  };
-
   return (
     <div className='contact'>
       <section className='title-page-contact' style={{backgroundImage: `url(${LazyBackgroundImage})`}}>
@@ -59,29 +27,10 @@ const Contact = () => {
           </button>
         </div>
         <div className='contact-form'>
-          <form onSubmit={sendEmail}>
-            <div className="form-group">
-              <label htmlFor="name">Nome</label>
-              <input type="text" className="form-control" id="name" name="from_name" onChange={(e) => setName(e.target.value)} value={name} required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">E-mail</label>
-              <input type="email" className="form-control" id="email" name="reply_to" onChange={(e) => setEmail(e.target.value)} value={email} required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Messaggio</label>
-              <textarea className="form-control" id="message" rows="3" name="message" onChange={(e) => setMessage(e.target.value)} value={message} required></textarea>
-            </div>
-            <button type="submit" className="btn btn-white" disabled={isSubmitting}>
-              {isSubmitting ? 'Invio in corso...' : 'Invia'}
-            </button>
-            {submitStatus === 'success' && <div className="success-message">Mensagem enviada com sucesso!</div>}
-            {submitStatus === 'error' && <div className="error-message">Erro ao enviar mensagem. Por favor, tente novamente mais tarde.</div>}
-          </form>
         </div>
       </section>
     </div>
   );
-};
+}
 
 export default Contact;
--- src/app/pages/home/home.js
+++ src/app/pages/home/home.js
@@ -5,6 +5,8 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import LazyBackgroundImage from './../../assets/background.webp';
 import LazyBackgroundImage2 from './../../assets/background2.webp';
+import LazyBackgroundImage3 from './../../assets/service3.webp';
+import LazyBackgroundImage4 from './../../assets/service4.webp';
 import './../../styles/home.css';
 
 const Home = () => {
@@ -33,8 +35,26 @@ const Home = () => {
         <div className='title-page-home'>
           <div className='slide-content' style={{backgroundImage: `url(${LazyBackgroundImage2})`}}>
             <div className='container-fluid'>
-              <h2>Benvenuti</h2>
-              <p>Dove ogni progetto si trasforma in un'opera d'arte. Sono Fabriccio Martinelli, il vostro guida nell'esplorare il potenziale creativo e tecnologico.</p>
+              <h2>Sviluppo di Siti Web e Sistemi</h2>
+              <p>Dalle idee alla realtà, offriamo soluzioni su misura per siti web e sistemi. Tecnologie all'avanguardia e progetti ottimizzati per un'esperienza utente impeccabile.</p>
+              <Link to="/servizi" className="btn btn-trans btn-big">Inizia</Link>
+            </div>
+          </div>
+        </div>
+        <div className='title-page-home'>
+          <div className='slide-content' style={{backgroundImage: `url(${LazyBackgroundImage3})`}}>
+            <div className='container-fluid'>
+              <h2>Marketing Sociale</h2>
+              <p>Il nostro team esperto ti aiuta a promuovere il tuo brand sui social media. Contenuti coinvolgenti, campagne mirate e analisi dei dati per il successo online.</p>
+              <Link to="/servizi" className="btn btn-trans btn-big">Inizia</Link>
+            </div>
+          </div>
+        </div>
+        <div className='title-page-home'>
+          <div className='slide-content' style={{backgroundImage: `url(${LazyBackgroundImage4})`}}>
+            <div className='container-fluid'>
+              <h2>Design graphico</h2>
+              <p>Dal logo ai materiali promozionali, creiamo un'identità visiva distintiva per il tuo marchio. Fatti notare e lascia un'impressione indelebile con il nostro servizio di design grafico e branding.</p>
               <Link to="/servizi" className="btn btn-trans btn-big">Inizia</Link>
             </div>
           </div>
