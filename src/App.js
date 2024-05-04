44692a2fe3b5374248b739babb687107eb267524
fabricciobm <fabricciobm@outlook.com>
2024-05-04 19:39:50 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,24 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from '../src/app/components/header/header';
 import Footer from '../src/app/components/footer/footer';
-import Home from '../src/app/pages/home/home';
-import Servizi from '../src/app/pages/servizi/servizi';
-import Contact from '../src/app/pages/contact/contact';
-import NotFound from './notfound';
+import AppRouter from '../src/AppRouter';
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/servizi/*" element={<Servizi />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+  <AppRouter />
   );
 }
 
---
+++ src/AppRouter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../src/app/pages/home/home';
+import Servizi from '../src/app/pages/servizi/servizi';
+import Contact from '../src/app/pages/contact/contact';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/servizi/*" element={<Servizi />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
