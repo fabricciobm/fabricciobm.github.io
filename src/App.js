57a3172c23e7f75657b347f18697315d6ca4ab9b
fabricciobm <fabricciobm@outlook.com>
2024-05-04 19:56:06 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from '../src/app/components/header/header';
 import Footer from '../src/app/components/footer/footer';
-import AppRouter from '../src/AppRouter';
+import Home from '../src/app/pages/home/home';
+import Servizi from '../src/app/pages/servizi/servizi';
+import Contact from '../src/app/pages/contact/contact';
+import NotFound from './notfound';
 function App() {
   return (
-  <AppRouter />
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/servizi/*" element={<Servizi />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from '../src/app/components/header/header';
+import Footer from '../src/app/components/footer/footer';
 import Home from '../src/app/pages/home/home';
 import Servizi from '../src/app/pages/servizi/servizi';
 import Contact from '../src/app/pages/contact/contact';
