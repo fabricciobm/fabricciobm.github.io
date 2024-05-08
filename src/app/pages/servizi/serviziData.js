5d37a97fc72961864c1ff62fc4037d3a135ed967
fabricciobm <fabricciobm@outlook.com>
2024-05-08 21:30:03 +0200

    update

--- src/app/pages/servizi/serviziData.js
+++ src/app/pages/servizi/serviziData.js
@@ -20,9 +20,9 @@ const Services = [
       planos: {
         plano1: { 
           nome: 'Design logo', 
-          destaque: ["5 Disegni diversi", "2 Revisioni", "Palette color", "1 Design finale", "Arte finale: JPG, PNG, PDF, CDR, AI, PS", "Consegna entro 5 giorni"], 
+          destaque: ["5 Disegni diversi", "Fino a 3 Revisioni", "Palette color", "1 Branding finale", "Formato: PNG, PDF, CDR, AI, PS", "Consegna entro 5 giorni"], 
           tags: ['design', 'logo', 'branding'],
-          price: 50 ,
+          price: 60 ,
         }
       }
     },
@@ -30,25 +30,25 @@ const Services = [
       image: serviceImage1,
       title: 'Siti Web Statici',
       description: "<p>Investi nel tuo successo online con fiducia. Con il nostro servizio di creazione siti web, hai a disposizione gli strumenti necessari per far crescere la tua presenza digitale e raggiungere nuovi traguardi. Contattaci oggi stesso per iniziare il tuo viaggio verso il successo online.</p>",
-      description_long: "<h2>Ottimizza la tua presenza online</h2><p>Massimizza la tua presenza online e raggiungi nuovi orizzonti di successo con il nostro servizio di creazione siti web. Siamo qui per offrirti una soluzione completa, progettata per aumentare la tua visibilità sui motori di ricerca e attirare clienti sempre più numerosi.</p><h2>Funzionalità garantite</h2><ul><li><strong>Ottimizzazione SEO inclusa:</strong> Fatti trovare più facilmente su Google e altri motori di ricerca.</li><li><strong>Design responsivo:</strong> Assicurati che il tuo sito sia fruibile su ogni dispositivo.</li><li><strong>Ottimizzazione della velocità:</strong> Nessuno vuole aspettare che un sito si carichi.</li><li><strong>Configurazione hosting:</strong> Dimentica i problemi legati all'hosting del tuo sito.</li></ul>",
+      description_long: "<h2>Ottimizza la tua presenza online</h2><p>Massimizza la tua presenza online e raggiungi nuovi orizzonti di successo con il nostro servizio di creazione siti web. Siamo qui per offrirti una soluzione completa, progettata per aumentare la tua visibilità sui motori di ricerca e attirare clienti sempre più numerosi.</p><h2>Funzionalità garantite</h2><ul><li><strong>Ottimizzazione SEO inclusa:</strong> Fatti trovare più facilmente su Google e altri motori di ricerca.</li><li><strong>Design responsivo:</strong> Assicurati che il tuo sito sia fruibile su ogni dispositivo.</li><li><strong>Ottimizzazione della velocità:</strong> Nessuno vuole aspettare che un sito si carichi.</li></ul>",
       destaque: [],
-      url: '/sviluppo-siti-web-funzionali',
+      url: '/sviluppo-siti-web-statici',
       tags: ['design', 'sviluppo'],
       planos: {
         plano1: { 
           nome: 'Pacchetto Base', 
-          destaque: ['1 Pagina', '5 Sessioni','2 Revisioni', 'Caricamento del contenuto incluso', 'Consegna in 3 giorni'], 
-          price: 100 
+          destaque: ['Sito funzionale', 'Fino a 5 Pagine', 'Caricamento del contenuto','Formulario di contato', 'Social icons', 'Configurazione dominio e hosting', 'Fino a 3 Revisioni', 'Consegna in 5 giorni'], 
+          price: 122,
         },
         plano2: { 
           nome: 'Pacchetto Silver', 
-          destaque: ['5 pagine', 'Sessioni ilimitate', '3 Revisioni','Caricamento del contenuto incluso', 'Catalogo di prodotti e servizi', 'Checkout Whatsapp', 'Consegna in 5 giorni'],
-          price: 250 ,
+          destaque: ['Sito funzionale', 'Fino a 10 Pagine', 'Caricamento del contenuto', 'Formulario di contato', 'Social icons', 'Catalogo prodotti/servizi', 'Checkout con whatsapp', 'Configurazione dominio e hosting', 'Fino a 4 Revisioni', 'Consegna in 7 giorni'], 
+          price: 315
         },
         plano3: { 
           nome: 'Pacchetto Premium', 
-          destaque: ['Pagine illimitate', 'Sessioni ilimitate', 'Revisioni ilimitate', 'Caricamento del contenuto incluso',   'Catalogo di prodotti e servizi', 'Checkout Whatsapp', 'Consegna in 7 giorni'],
-          price: 500 
+          destaque: ['Sito funzionale', 'Pagine ilimitate', 'Caricamento del contenuto', 'Formulario di contato', 'Social icons', 'Catalogo prodotti/servizi', 'Checkout con whatsapp', 'Configurazione dominio e hosting', 'Configurazione Chatbot', 'Revisioni ilimitate', 'Consulenza Online (12h/anno)', 'Consegna in 7 giorni'],
+          price: 1082
         },
       },
     },
@@ -56,25 +56,25 @@ const Services = [
       image: serviceImage7,
       title: 'Siti Web Dinamici',
       description: "<p>Porta la tua presenza online al livello successivo con i nostri siti web dinamici. Grazie alla nostra piattaforma avanzata, potrai creare un'esperienza coinvolgente e interattiva per i tuoi visitatori. Dalla personalizzazione dei contenuti alla gestione intuitiva, i nostri siti dinamici offrono una soluzione completa per soddisfare le esigenze del tuo business. Contattaci oggi stesso per scoprire come possiamo trasformare la tua presenza digitale.</p>",
-      description_long: "<p>Con i nostri siti web dinamici, non solo avrai un bell'aspetto, ma potrai anche offrire un'esperienza utente straordinaria. Dai un'occhiata alle nostre funzionalità chiave:</p><ul><li><strong>Pagine Personalizzate:</strong> Crea contenuti dinamici e personalizzati per ogni tipo di visitatore.</li><li><strong>Interattività Avanzata:</strong> Dai ai tuoi utenti la possibilità di interagire con il tuo sito attraverso formulari, chat e altro ancora.</li><li><strong>Gestione Contenuti Intuitiva:</strong> Aggiorna e gestisci facilmente il tuo sito senza dover essere un esperto di programmazione.</li><li><strong>Ottimizzazione SEO Integrata:</strong> Assicurati che il tuo sito sia facilmente trovabile sui motori di ricerca.</li><li><strong>Analisi e Monitoraggio:</strong> Monitora le prestazioni del tuo sito e ottieni insights preziosi per migliorare continuamente la tua strategia online.</li><li><strong>Assistenza Tecnica:</strong> Il nostro team esperto è qui per aiutarti con qualsiasi problema tecnico e per garantire che il tuo sito funzioni sempre al meglio.</li></ul>",
+      description_long: "<p>Con i nostri siti web dinamici, non solo avrai un bell'aspetto, ma potrai anche offrire un'esperienza utente straordinaria. Dai un'occhiata alle nostre funzionalità chiave:</p><ul><li><strong>Pagine Personalizzate:</strong> Crea contenuti dinamici e personalizzati per ogni tipo di visitatore.</li><li><strong>Interattività Avanzata:</strong> Dai ai tuoi utenti la possibilità di interagire con il tuo sito attraverso formulari, chat e altro ancora.</li><li><strong>Gestione Contenuti Intuitiva:</strong> Aggiorna e gestisci facilmente il tuo sito senza dover essere un esperto di programmazione.</li><li><strong>Ottimizzazione SEO Integrata:</strong> Assicurati che il tuo sito sia facilmente trovabile sui motori di ricerca.</li><li><strong>Analisi e Monitoraggio:</strong> Monitora le prestazioni del tuo sito e ottieni insights preziosi per migliorare continuamente la tua strategia online.</li></ul>",
       destaque: [],
       url: '/sviluppo-siti-web-dinamici',
       tags: ['design', 'sviluppo'],
       planos: {
         plano1: { 
           nome: 'Pacchetto Base', 
-          destaque: ['Pagine ilimitate', 'Spazio 5Gb Totale', 'Caricamento del contenuto incluso', 'Icone dei social media', 'Formulario di contatto', 'Registrazione utenti', 'Consegna in 7 giorni'],
-          price: 600 
+          destaque: ['Sito funzionante', 'Facile gestione', 'Pagine ilimitate', 'Caricamento del contenuto', 'blog/news', 'Social share buttons', 'Formulario di contatto', 'Gestione utenti','Configurazione dominio e hosting', 'Consegna in 7 giorni'],
+          price: 475
         },
         plano2: { 
           nome: 'Pacchetto Silver', 
-          destaque: ['Pagine ilimitate', 'Spazio 5Gb Totale', 'Caricamento del contenuto incluso', 'Icone dei social media', 'Formulario di contatto', 'Registrazione utenti', 'Consegna in 7 giorni'],
-          price: 900 
+          destaque: ['Sito funzionante', 'Facile gestione', 'Pagine ilimitate', 'Caricamento del contenuto', 'blog/news', 'Social share buttons', 'Formulario di contatto', 'Gestione utenti','Configurazione dominio e hosting', 'backup 1/7','Consegna in 7 giorni'],
+          price: 750 
         },
         plano3: { 
           nome: 'Pacchetto Premium', 
-          destaque: ['Pagine ilimitate', 'Spazio 10Gb Totale', 'Caricamento del contenuto incluso', 'Icone dei social media',  'Formulario di contatto', 'Registrazione utenti', 'Consegna in 5 giorni', 'VIP Support'],
-          price: 1200 
+          destaque: ['Sito funzionante', 'Facile gestione', 'Pagine ilimitate', 'Caricamento del contenuto', 'blog/news', 'Social share buttons', 'login sociale', 'Formulario di contatto', 'Gestione utenti','Configurazione dominio e hosting', 'backup 7/7', 'Consulenza Online (12h/anno)', 'Consegna in 7 giorni'],
+          price: 1525
         },
       },
     },
